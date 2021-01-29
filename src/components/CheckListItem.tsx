2d82ecfd306af1741b4056150d3bae7eb1a4a533
Sergey Dubovyk <knidarkness@gmail.com>
2021-01-30 01:27:29 +0200

    chore: test signing

--- src/components/CheckListItem.tsx
+++ src/components/CheckListItem.tsx
@@ -4,8 +4,8 @@ import { ChecklistItem, PilotRole } from '../model/types';
 export default function CheckListItem({ checklistItem }: { checklistItem: ChecklistItem }) {
   return (
     <div>
-      <span>{checklistItem.actionName}</span>
-      <span>{checklistItem.actionTodo}</span>
+      <span>{checklistItem.actionName} </span>
+      <span>{checklistItem.actionTodo} </span>
       <span>{checklistItem.actor === PilotRole.PILOT_FLYING ? 'PF' : 'PM'}</span>
     </div>
   );
